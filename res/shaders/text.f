d9f2d89db5c90552ebe7906144c69701bd391509
zakcisan <37312022+zakcisan@users.noreply.github.com>
2018-07-16 11:58:04 -0400

    crap i'm dumb

--- res/shaders/text.f
+++ res/shaders/text.f
@@ -5,5 +5,5 @@ uniform vec4 color;
 
 void main()
 {
-    gl_FragColor = color * vec4(1.0, 1.0, 1.0, texture(tex, texCoord).r);
+    gl_FragColor = color * vec4(1.0, 1.0, 1.0, texture2D(tex, texCoord).r);
 }
